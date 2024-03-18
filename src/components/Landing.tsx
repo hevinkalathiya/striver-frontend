cf45c3306fda11a6a66dacadc1f73b01870abb4d
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-03-18 21:28:34 +0530

    first commit

---
+++ src/components/Landing.tsx
@@ -0,0 +1,258 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import OutputWindow from "./OutputWindow";
+import CustomInput from "./CustomInput";
+import OutputDetails from "./OutputDetails";
+import ThemeDropdown from "./ThemeDropdown";
+import LanguagesDropdown from "./LanguagesDropdown";
+import { languageOptions } from "@/constants/languageOptions";
+import CodeEditorWindow from "./CodeEditorWindow";
+import { cn } from "@/lib/utils";
+import useKeyPress from "@/hooks/useKeyPress";
+import { Input } from "./ui/input";
+
+interface OutputDetailsType {
+  status?: { id: number };
+  token?: string;
+}
+
+const javascriptDefault = `
+const target = 5;
+console.log(target);
+`;
+
+const Landing: React.FC = () => {
+  const [code, setCode] = useState<string>(javascriptDefault);
+  const [customInput, setCustomInput] = useState<string>("");
+  const [outputDetails, setOutputDetails] = useState<OutputDetailsType | null>(
+    null
+  );
+  const [processing, setProcessing] = useState<boolean | null>(null);
+  const [theme, setTheme] = useState("cobalt");
+  const [language, setLanguage] = useState(languageOptions[0]);
+  const [username, setUsername] = useState<string>("");
+
+  const onSelectChange = (sl: any) => {
+    console.log("selected Option...", sl);
+    setLanguage(sl);
+  };
+
+  const enterPress = useKeyPress("Enter");
+  const ctrlPress = useKeyPress("Control");
+
+  useEffect(() => {
+    if (enterPress && ctrlPress) {
+      console.log("enterPress", enterPress);
+      console.log("ctrlPress", ctrlPress);
+      handleCompile();
+    }
+  }, [ctrlPress, enterPress]);
+
+  const onChange = (action: string, data: string) => {
+    switch (action) {
+      case "code": {
+        setCode(data);
+        break;
+      }
+      default: {
+        console.warn("case not handled!", action, data);
+      }
+    }
+  };
+
+  console.log({
+    language_id: language.id,
+    source_code: btoa(code),
+    stdin: btoa(customInput),
+  });
+
+  const handleCompile = () => {
+    axios
+      .post("https://striver-backend-1.onrender.com/api/code", {
+        username,
+        code,
+        codeLanguage: language.label,
+        std: customInput,
+      })
+      .then((response) => {
+        console.log("User data saved successfully:", response.data);
+        showSuccessToast(`User data saved successfully!`);
+      })
+      .catch((error) => {
+        console.error("Error saving user data:", error);
+      });
+
+    setProcessing(true);
+    const formData = {
+      language_id: language.id,
+      source_code: btoa(code),
+      stdin: btoa(customInput),
+    };
+    const options = {
+      method: "POST",
+      url: "https://judge0-ce.p.rapidapi.com/submissions",
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "content-type": "application/json",
+        "Content-Type": "application/json",
+        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+        "X-RapidAPI-Key": "9a90edca60msh32c2c90181ae33cp17c2acjsn9c9bc7812445",
+      },
+      data: formData,
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log("res.data", response.data);
+        const token = response.data.token;
+        checkStatus(token);
+      })
+      .catch((err) => {
+        let error = err.response ? err.response.data : err;
+        // get error status
+        let status = err.response.status;
+        console.log("status", status);
+        if (status === 429) {
+          console.log("too many requests", status);
+
+          showErrorToast(
+            `Quota of 100 requests exceeded for the Day! Please read the blog on freeCodeCamp to learn how to setup your own RAPID API Judge0!`,
+            10000
+          );
+        }
+        setProcessing(false);
+        console.log("catch block...", error);
+      });
+  };
+
+  const checkStatus = async (token: string) => {
+    const options = {
+      method: "GET",
+      url: "https://judge0-ce.p.rapidapi.com/submissions" + "/" + token,
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+        "X-RapidAPI-Key": "9a90edca60msh32c2c90181ae33cp17c2acjsn9c9bc7812445",
+      },
+    };
+    try {
+      let response = await axios.request(options);
+      let statusId = response.data.status?.id;
+
+      // Processed - we have a result
+      if (statusId === 1 || statusId === 2) {
+        // still processing
+        setTimeout(() => {
+          checkStatus(token);
+        }, 2000);
+        return;
+      } else {
+        setProcessing(false);
+        setOutputDetails(response.data);
+        showSuccessToast(`Compiled Successfully!`);
+        console.log("response.data", response.data);
+        return;
+      }
+    } catch (err) {
+      console.log("err", err);
+      setProcessing(false);
+      showErrorToast();
+    }
+  };
+
+  const handleThemeChange = (th: string) => {
+    console.log("theme...", th);
+    setTheme(th);
+  };
+
+  const showSuccessToast = (msg?: string) => {
+    toast.success(msg || `Compiled Successfully!`, {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  const showErrorToast = (msg?: string, timer?: number) => {
+    toast.error(msg || `Something went wrong! Please try again.`, {
+      position: "top-right",
+      autoClose: timer ? timer : 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+
+      <div className="flex flex-row ">
+        <div className="px-4 py-2">
+          <LanguagesDropdown onSelectChange={onSelectChange} />
+        </div>
+        <div className="px-4 py-2">
+          <ThemeDropdown handleThemeChange={handleThemeChange} theme={theme} />
+        </div>
+        <div className="px-4 py-2">
+          <input
+            className="rounded-[8px] border-2 border-black px-4 h-full"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            placeholder="@Striver"
+          />
+        </div>
+      </div>
+      <div className="flex flex-row space-x-4 items-start px-4 py-4">
+        <div className="flex flex-col  w-[70%] h-full justify-start items-end">
+          <CodeEditorWindow
+            code={code}
+            onChange={onChange}
+            language={language?.value}
+            theme={theme}
+          />
+        </div>
+
+        <div className="right-container flex flex-shrink-0 w-[30%] flex-col">
+          <OutputWindow outputDetails={outputDetails} />
+          <div className="flex flex-col items-end">
+            <CustomInput
+              customInput={customInput}
+              setCustomInput={setCustomInput}
+            />
+            <button
+              onClick={handleCompile}
+              disabled={!code}
+              className={cn(
+                "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
+                !code ? "opacity-50" : ""
+              )}
+            >
+              {processing ? "Processing..." : "Compile and Execute"}
+            </button>
+          </div>
+          {outputDetails && <OutputDetails outputDetails={outputDetails} />}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Landing;
