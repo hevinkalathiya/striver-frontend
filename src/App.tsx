5061f70ca0e6491223453d162ac7e0be0adbdc22
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-03-21 11:42:29 +0530

    Add API call on App component initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
 import "./App.css";
 import Landing from "./components/Landing";
 import Table from "./Table";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios from "axios";
 
 function App() {
+  // !Api call only for start server
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://striver-backend-1.onrender.com/api/code"
+        );
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="max-w-full mx-auto">
       <BrowserRouter>
